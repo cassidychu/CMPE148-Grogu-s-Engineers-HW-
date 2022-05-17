87c215dbd9b880910a224029372da3977a8dd5ab
Andy Ngo <Andyngo28@gmail.com>
2022-05-17 01:47:05 -0700

    Fixed the code and should provide correct results

--- HW 3 Lab.py
+++ HW 3 Lab.py
@@ -2,7 +2,7 @@ from socket import *
 msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
 
-print ('Test')
+print('Test')
 # Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = "smtp.gmail.com"
 mailport = 587
@@ -12,41 +12,41 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((mailserver, mailport))
 
 recv = clientSocket.recv(1024)
-print (recv)
+print(recv)
 if recv[:3] != '220':
-	print ('220 reply not received from server.')
+    print('220 reply not received from server.')
 
 # Send HELO command and print server response.
 heloCommand = 'HELO Alice\r\n'
 clientSocket.send(heloCommand)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '250':
-	print ('250 reply not received from server.')
+    print('250 reply not received from server.')
 
 # Send MAIL FROM command and print server response.
 mailfromCommand = 'MAIL FROM: asdf888@gmail.com\r\n'
 clientSocket.send(mailfromCommand)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '250':
-	print ('250 reply not received from server.')
+    print('250 reply not received from server.')
 
 # Send RCPT TO command and print server response.
 rcpttoCommand = 'RCPT TO: alex.t.montgomery@gmail.com\r\n'
 clientSocket.send(rcpttoCommand)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '250':
-	print ('250 reply not received from server.')
+    print('250 reply not received from server.')
 
 # Send DATA command and print server response.
 dataCommand = 'DATA\r\n'
 clientSocket.send(dataCommand)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '354':
-	print ('354 reply not received from server.')
+    print('354 reply not received from server.')
 
 # Send message data.
 clientSocket.send(msg)
@@ -54,15 +54,15 @@ clientSocket.send(msg)
 # Message ends with a single period.
 clientSocket.send(endmsg)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '250':
-	print ('250 reply not received from server.')
+    print('250 reply not received from server.')
 
 # Send QUIT command and get server response.
-print ('Right before Quit')
+print('Right before Quit')
 quitCommand = 'QUIT\r\n'
 clientSocket.send(quitCommand)
 recv1 = clientSocket.recv(1024)
-print (recv1)
+print(recv1)
 if recv1[:3] != '221':
-	print ('221 reply not received from server.')
+    print('221 reply not received from server.')
--- hw 2/web_server.py
+++ hw 2/web_server.py
@@ -1,38 +1,43 @@
-#import socket module
+# import socket module
 from socket import *
-import sys # In order to terminate the program
-serverSocket = socket(AF_INET, SOCK_STREAM) #Prepare a sever socket
+import sys  # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM)  # Prepare a sever socket
 #Fill in start
-server = 6789
-serverSocket.bind(('',server))
+
+serverSocket.bind(('', 6789))
 serverSocket.listen(1)
 #Fill in end
 while True:
-   #Establish the connection 
-   print('Ready to serve...') 
-   connectionSocket, addr = serverSocket.accept() #Fill in start #Fill in end
-   try:
-      message = connectionSocket.recv(1024) #Fill in start #Fill in end 
-      filename = message.split()[1]
-      f = open(filename[1:])
-      outputdata = f.read() #Fill in start #Fill in end 
-      #Send one HTTP header line into socket
-      #Fill in start
-      connectionSocket.send('\nHTTP/1.1 400 Bad Request\n\n'.encode())
-      #Fill in end
-      #Send the content of the requested file to the client 
-      for i in range(0, len(outputdata)):
-         connectionSocket.send(outputdata[i].encode()) 
-         connectionSocket.send("\r\n".encode())
-         connectionSocket.close() 
-   except IOError:
-#Send response message for file not found
-    #Fill in start
-    connectionSocket.send('\nHTTP/1.1 404 Error: File Not Found\n\n'.enconde())
-   #Fill in end
-#Close client socket
-      #Fill in start
-connectionSocket.close()
-      #Fill in end
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        # print(str(message))
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        # Send one HTTP header line into socket
+        #Fill in start
+
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+
+        #Fill in end
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        #Fill in start
+        connectionSocket.send(
+            '\nHTTP/1.1 404 Error: File Not Found\n\n'.encode())
+    #Fill in end
+# Close client socket
+        #Fill in start
+        connectionSocket.close()
+#Fill in end
 serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+sys.exit()  # Terminate the program after sending the corresponding data
